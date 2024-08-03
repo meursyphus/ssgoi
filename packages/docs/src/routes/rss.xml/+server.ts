aea89734bfe67134ade48c610d91228d5bc04ca5
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-08-03 16:27:13 +0900

    asdf

--- packages/docs/src/routes/rss.xml/+server.ts
+++ packages/docs/src/routes/rss.xml/+server.ts
@@ -1,8 +1,6 @@
 import * as config from '$lib/config'
 import type { Post } from '$lib/types'
 
-export const prerender = true
-
 export async function GET({ fetch }) {
 	const response = await fetch('api/posts')
 	const posts: Post[] = await response.json()
