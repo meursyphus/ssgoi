c5c36c6b3263d6fcae2ec730d596b433e93f347b
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-07-22 09:29:25 +0900

    temp

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,7 +1,18 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
+
+export default config;
--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,48 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    svelte({
-      compilerOptions: {
-        runes: true
-      },
-      prebundleSvelteLibraries: true,
-    }),
-    dts({
-      insertTypesEntry: true,
-      outDir: 'dist',
-      include: ['src/lib/**/*'],
-      exclude: ['src/vite-env.d.ts'],
-      tsconfigPath: './tsconfig.app.json',
-      beforeWriteFile: (filePath, content) => ({
-        filePath: filePath.replace('/src/lib', ''),
-        content,
-      }),
-    })
-  ],
-  build: {
-    lib: {
-      entry: {
-        'index': resolve(__dirname, 'src/lib/index.ts'),
-        'transitions/index': resolve(__dirname, 'src/lib/transitions/index.ts'),
-        'view-transitions/index': resolve(__dirname, 'src/lib/view-transitions/index.ts'),
-        'types': resolve(__dirname, 'src/lib/types.ts'),
-      },
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['svelte'],
-      output: {
-        globals: {
-          svelte: 'Svelte',
-        },
-        interop: 'auto',
-      },
-    },
-    ssr: true,
-    ssrEmitAssets: true,
-  },
-})
+	plugins: [sveltekit()]
+});
