58cec4bf749e6275854bd3dd2380d85e0e7afbe2
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-07-22 12:31:38 +0900

    good

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,40 +1,6 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    svelte(),
-    dts({
-      insertTypesEntry: true,
-      outDir: "dist",
-      include: ["src/lib/**/*"],
-      exclude: ["src/vite-env.d.ts"],
-      tsconfigPath: "./tsconfig.app.json",
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-      beforeWriteFile: (filePath, content) => ({
-        filePath: filePath.replace("/src/lib", ""),
-        content,
-      }),
-    }) as any,
-  ],
-  // ssr: {
-  //   noExternal: ["@ssgoi/core"],
-  //   external: ["svelte"],
-  // },
-  build: {
-    lib: {
-      entry: {
-        index: resolve(__dirname, "src/lib/index.ts"),
-        transitions: resolve(__dirname, "src/lib/transitions/index.ts"),
-        "view-transitions": resolve(
-          __dirname,
-          "src/lib/view-transitions/index.ts"
-        ),
-        types: resolve(__dirname, "src/lib/types.ts"),
-      },
-      formats: ["es", "cjs"],
-    },
-  },
+export default defineConfig({
+	plugins: [sveltekit()]
 });
