c3e2875a40e7cdb79b2da2d35b8c41f9e2dcc1d8
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-07-22 11:53:15 +0900

    temp

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -1,39 +1,40 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(),
+  plugins: [
+    svelte(),
     dts({
       insertTypesEntry: true,
-      outDir: 'dist',
-      include: ['src/lib/**/*'],
-      exclude: ['src/vite-env.d.ts'],
-      tsconfigPath: './tsconfig.app.json',
+      outDir: "dist",
+      include: ["src/lib/**/*"],
+      exclude: ["src/vite-env.d.ts"],
+      tsconfigPath: "./tsconfig.app.json",
+
       beforeWriteFile: (filePath, content) => ({
-        filePath: filePath.replace('/src/lib', ''),
+        filePath: filePath.replace("/src/lib", ""),
         content,
       }),
-    }) as any
+    }) as any,
   ],
-   build: {
+  // ssr: {
+  //   noExternal: ["@ssgoi/core"],
+  //   external: ["svelte"],
+  // },
+  build: {
     lib: {
       entry: {
-        'index': resolve(__dirname, 'src/lib/index.ts'),
-        'transitions': resolve(__dirname, 'src/lib/transitions/index.ts'),
-        'view-transitions': resolve(__dirname, 'src/lib/view-transitions/index.ts'),
-        'types': resolve(__dirname, 'src/lib/types.ts'),
+        index: resolve(__dirname, "src/lib/index.ts"),
+        transitions: resolve(__dirname, "src/lib/transitions/index.ts"),
+        "view-transitions": resolve(
+          __dirname,
+          "src/lib/view-transitions/index.ts"
+        ),
+        types: resolve(__dirname, "src/lib/types.ts"),
       },
-      formats: ['es', 'cjs'],
+      formats: ["es", "cjs"],
     },
-    rollupOptions: {
-      external: ["svelte"],
-      output: {
-        globals: {
-          svelte: 'svelte',
-        },
-      }
-    }
   },
-})
+});
