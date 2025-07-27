3386232a649f2e3a25d5d65c82b5c0441ef6e80a
zerodice0 <zerodice0@gmail.com>
2025-07-28 08:46:24 +0900

    feat: 사용자가 키값을 신경쓰지 않아도 singleton 생성자를 만들 수 있도록 수정
    - https://github.com/aigoncharov/singleton 을 참조.
    - clear() 및 자원 정리에 대한 고려 추가.
    - 사용자가 커스텀 키를 사용할 때에 대한 고려 추가.
    - 키 혹은 Object를 기반으로 접근하는 경우에 대한 고려 추가.

--- packages/core/src/lib/singleton.ts
+++ packages/core/src/lib/singleton.ts
@@ -15,39 +15,53 @@ export class Singleton {
     return global[WORKSPACE_SYMBOL];
   }
 
-  // 1. offer value
-  public static get<T>(key: any, value: T, isOverwrite?: boolean): T;
-  // 2. only get value
-  public static get<T>(key: any): T | undefined;
-  // implement
-  public static get<T>(key: any, value?: T, isOverwrite: boolean = false): T | undefined {
-    const workspace = this.ensureWorkspace();
+  public static create<T extends new (...args: any[]) => any>(
+    Constructor: T,
+    isOverwrite: boolean = false,
+    customKey?: string
+  ): T {
+    return new Proxy(Constructor, {
+      construct(target, argumentList, newTarget) {
+        const workspace = Singleton.ensureWorkspace();
+        const key = Symbol.for(customKey || target.name);
 
-    if (value !== undefined) {
-      console.log('isOverwrite: ', isOverwrite);
+        if (!workspace.has(key) || isOverwrite) {
+          const instance = Reflect.construct(target, argumentList, newTarget);
+          workspace.set(key, instance);
+        }
 
-      if (!workspace.has(key) || isOverwrite) {
-        workspace.set(key, value);
+        return workspace.get(key);
       }
-
-      return workspace.get(key);
-    } else {
-      return workspace.get(key);
-    }
+    });
   }
 
-  public static has(key: any): boolean {
-    const workspace = this.ensureWorkspace();
+  public static get(keyOrObject: Object|string): Object|undefined {
+    const workspace = Singleton.ensureWorkspace();
+    const isKey = typeof keyOrObject === 'string';
+    const key = isKey ? Symbol.for(keyOrObject) : Symbol.for(keyOrObject.constructor.name);
+
+    return workspace.get(key);
+  }
+
+  public static has(keyOrObject: Object|string): boolean {
+    const workspace = Singleton.ensureWorkspace();
+    const isKey = typeof keyOrObject === 'string';
+    const key = isKey ? Symbol.for(keyOrObject) : Symbol.for(keyOrObject.constructor.name);
+
     return workspace.has(key);
   }
 
-  public static remove(key: any): boolean {
-    const workspace = this.ensureWorkspace();
+  public static remove(keyOrObject: Object|string): boolean {
+    const workspace = Singleton.ensureWorkspace();
+    const isKey = typeof keyOrObject === 'string';
+    const key = isKey ? Symbol.for(keyOrObject) : Symbol.for(keyOrObject.constructor.name);
+
     return workspace.delete(key);
   }
 
   public static clear(): void {
-    const workspace = this.ensureWorkspace();
+    const workspace = Singleton.ensureWorkspace();
+
     workspace.clear();
   }
 }
