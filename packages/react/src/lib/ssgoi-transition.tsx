ecc647e874c3d9073d1a58a13695683bee7ad5cb
hmyo2853 <hmyo2853@gmail.com>
2025-08-22 17:03:53 +0900

    feat: add className and as props to React SsgoiTransition

--- packages/react/src/lib/ssgoi-transition.tsx
+++ packages/react/src/lib/ssgoi-transition.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import type { ReactNode } from "react";
+import type { ReactNode, ElementType } from "react";
 import { transition } from "./transition";
 import { useSsgoi } from "./context";
 
-export const SsgoiTransition = ({
-  children,
-  id,
-}: {
+type SsgoiTransitionProps<T extends ElementType = "div"> = {
   children: ReactNode;
   id: string;
-}) => {
+  as?: T;
+  className?: string;
+};
+
+export const SsgoiTransition = <T extends ElementType = "div">({
+  children,
+  id,
+  as,
+  className,
+  ...rest
+}: SsgoiTransitionProps<T>) => {
   const getTransition = useSsgoi();
+  const Component = as || "div";
 
   return (
-    <div ref={transition(getTransition(id))} data-ssgoi-transition={id}>
+    <Component 
+      ref={transition(getTransition(id))} 
+      data-ssgoi-transition={id}
+      className={className}
+      {...rest}
+    >
       {children}
-    </div>
+    </Component>
   );
 };
