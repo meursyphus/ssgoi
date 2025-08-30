fe5fa7f20a43c7e37dfaa923b1387976415e7ad2
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-08-30 10:45:04 +0900

    temp

--- packages/core/src/lib/utils/get-rect.ts
+++ packages/core/src/lib/utils/get-rect.ts
@@ -1,6 +1,11 @@
 /**
  * Get the relative position of an element to its root container
  * Returns a DOMRect with position relative to the root element
+ * 
+ * TODO: When both root and el have transforms (e.g., rotate), the relative position
+ * might need compensation. Currently using simple bounding rect difference which
+ * works for most cases but may be inaccurate for complex 3D transforms.
+ * Consider adding transform matrix calculation if needed in the future.
  */
 export function getRect(root: HTMLElement, el: HTMLElement): DOMRect {
   const rootRect = root.getBoundingClientRect();
