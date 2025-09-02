ae0fbbb289271d085219a5c5dbac3aa9d0a70cf5
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-09-03 07:17:27 +0900

    good

--- packages/svelte/src/lib/index.ts
+++ packages/svelte/src/lib/index.ts
@@ -1,4 +1,4 @@
-export { transition } from './transition.js';
-export { default as Ssgoi } from './ssgoi.svelte';
-export { default as SsgoiTransition } from './ssgoi-transition.svelte';
-export * from './types.js';
+export { transition } from "./transition.js";
+export { default as Ssgoi } from "./ssgoi.svelte";
+export { default as SsgoiTransition } from "./ssgoi-transition.svelte";
+export * from "./types.js";
--- packages/svelte/src/lib/transition.ts
+++ packages/svelte/src/lib/transition.ts
@@ -1,29 +1,37 @@
-import { transition as _transition, type Transition, type TransitionKey } from '@ssgoi/core';
+import {
+  transition as _transition,
+  type Transition,
+  type TransitionKey,
+} from "@ssgoi/core";
 
 export const transition = <TAnimationValue = number>(
-	node: HTMLElement,
-	params: Transition<undefined, TAnimationValue> & { key?: TransitionKey }
+  node: HTMLElement,
+  params: Transition<undefined, TAnimationValue> & { key?: TransitionKey },
 ) => {
-	let callback = _transition<TAnimationValue>({
-		key: params?.key,
-		in: params?.in,
-		out: params?.out,
-		ref: node
-	});
-	let cleanup = callback(node);
+  let callback = _transition<TAnimationValue>({
+    key: params?.key,
+    in: params?.in,
+    out: params?.out,
+    ref: node,
+  });
+  let cleanup = callback(node);
 
-	return {
-		update(newParams: Transition<undefined, TAnimationValue> & { key?: TransitionKey }) {
-			callback = _transition<TAnimationValue>({
-				key: newParams?.key,
-				in: newParams?.in,
-				out: newParams?.out,
-				ref: node
-			});
-			cleanup = callback(node);
-		},
-		destroy() {
-			cleanup?.();
-		}
-	};
+  return {
+    update(
+      newParams: Transition<undefined, TAnimationValue> & {
+        key?: TransitionKey;
+      },
+    ) {
+      callback = _transition<TAnimationValue>({
+        key: newParams?.key,
+        in: newParams?.in,
+        out: newParams?.out,
+        ref: node,
+      });
+      cleanup = callback(node);
+    },
+    destroy() {
+      cleanup?.();
+    },
+  };
 };
