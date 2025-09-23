73b96b46d93cec730fcbd48392aaf1b0b285e2ef
Jaemin Cheon <41279460+elrion018@users.noreply.github.com>
2025-09-24 02:25:06 +0900

    refactor: Rename hero-rotate.ts to jaemin.ts
    Update file name to match transition name.
    Update exports in index.ts.

--- packages/core/src/lib/view-transitions/index.ts
+++ packages/core/src/lib/view-transitions/index.ts
@@ -2,7 +2,7 @@ export * from "./blind";
 export * from "./drill";
 export * from "./fade";
 export * from "./hero";
-export * from "./hero-rotate";
+export * from "./jaemin";
 export * from "./pinterest";
 export * from "./scroll";
 export * from "./slide";
