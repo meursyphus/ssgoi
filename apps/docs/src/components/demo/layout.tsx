d8879260afc34c329dd3cbc72c15f1e41a1ffe4d
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-11-08 15:44:13 +0900

    docs: no fade in demo

--- apps/docs/src/components/demo/layout.tsx
+++ apps/docs/src/components/demo/layout.tsx
@@ -62,7 +62,6 @@ export default function DemoLayout({ children }: DemoLayoutProps) {
 
   const config: SsgoiConfig = useMemo(
     () => ({
-      defaultTransition: fade(),
       transitions: [
         // Pinterest transitions
         {
