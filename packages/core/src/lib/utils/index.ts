3adc9a8d2bf6cfa6105b0bf854884ec99265c03b
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-12-14 16:19:55 +0900

    good

--- packages/core/src/lib/utils/index.ts
+++ packages/core/src/lib/utils/index.ts
@@ -1,7 +1,6 @@
 export { prepareOutgoing } from "./prepare-outgoing";
 export { getScrollingElement } from "./get-scrolling-element";
 export { getPositionedParent } from "./get-positioned-parent";
-export { parseCallerLocation } from "./parse-caller-location";
 export { sleep } from "./sleep";
 export { getRect } from "./get-rect";
 export { round, floor, ceil, toFixed } from "./number";
