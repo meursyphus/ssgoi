74826f17e3ba47a9e8d277069ceca40f5e0f784b
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2025-12-19 00:18:18 +0900

    remove restrict mode because of ref bug

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -13,36 +13,36 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/ssgoi.gif',
+        source: "/ssgoi.gif",
         headers: [
           {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
+            key: "Access-Control-Allow-Origin",
+            value: "*",
           },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET',
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
           },
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
           },
           {
-            key: 'Content-Type',
-            value: 'image/gif',
+            key: "Content-Type",
+            value: "image/gif",
           },
         ],
       },
       {
-        source: '/:path*.(gif|jpg|jpeg|png|svg|webp)',
+        source: "/:path*.(gif|jpg|jpeg|png|svg|webp)",
         headers: [
           {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
+            key: "Access-Control-Allow-Origin",
+            value: "*",
           },
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
           },
         ],
       },
--- apps/react-demo/next.config.ts
+++ apps/react-demo/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import SsgoiAutoKey from "@ssgoi/react/unplugin/webpack";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (config) => {
     config.plugins.push(SsgoiAutoKey());
     return config;
